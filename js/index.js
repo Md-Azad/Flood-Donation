17f9c3d33424556b2e1ec8ce4955eadc0213a2ae
Md Azad Hossain <sokalazad@gmail.com>
2024-09-24 02:53:22 +0200

    added background color during scrollY

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 document.getElementById("btn-donation").addEventListener("click", function () {
-  document.getElementById("donation-contant").classList.remove("hidden");
+  document.getElementById("donation-section").classList.remove("hidden");
   document.getElementById("history-contant").classList.add("hidden");
   document.getElementById("btn-donation").classList.add("bg-[#B4F461]");
   document.getElementById("btn-history").classList.remove("bg-[#B4F461]");
 });
 document.getElementById("btn-history").addEventListener("click", function () {
-  document.getElementById("donation-contant").classList.add("hidden");
+  document.getElementById("donation-section").classList.add("hidden");
   document.getElementById("history-contant").classList.remove("hidden");
   document.getElementById("btn-donation").classList.remove("bg-[#B4F461]");
   document.getElementById("btn-history").classList.add("bg-[#B4F461]");
+});
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    header.style.backgroundColor = "#7c96eb";
+    header.style.opacity = "0.8";
+    header.classList.add("shadow-lg");
+  } else {
+    header.style.backgroundColor = "transparent";
+    header.style.opacity = "1";
+    header.classList.remove("shadow-lg");
+  }
 });
 
 const cards = document.querySelectorAll(".donation-contant");
